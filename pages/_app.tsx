49bf5439bd2814e738db4266256c1850ade83ed6
PandaiCoding <sumari.w4@gmail.com>
2021-09-30 19:03:11 +0700

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import "tailwindcss/tailwind.css";
+
+import type { AppProps } from "next/app";
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
